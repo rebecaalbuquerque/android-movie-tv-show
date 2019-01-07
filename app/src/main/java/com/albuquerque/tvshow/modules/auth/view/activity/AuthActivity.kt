07ad26c8ddc5095e826b8d6996e79b9a5347c412
Rebeca <rebeca.albuquerque96@gmail.com>
2019-01-07 11:08:05 -0300

    First commit

---
+++ app/src/main/java/com/albuquerque/tvshow/modules/auth/view/activity/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.albuquerque.tvshow.modules.auth.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.albuquerque.tvshow.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
